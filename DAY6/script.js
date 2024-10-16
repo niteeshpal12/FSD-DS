7e0f1f2c3ea0578a9914d6e3981534314259aedf
niteeshpal12 <niteeshpal1762002@gmail.com>
2024-10-16 15:37:19 +0530

    flex

---
+++ DAY6/script.js
@@ -0,0 +1,14 @@
+let arr=[1,2,3,4,5,6,77];
+// let newary=arr.map((x)=>x*);
+// console.log(newary);
+// let even=arr.filter(x=>x%2==0);
+// console.log(even);
+// let sum=arr.reduce((x,y)=>x+y,0);
+// console.log(sum);
+
+let evensum=arr.filter((x)=>x%2==0).reduce((x,y)=>x+y,0);
+console.log(evensum)
+ 
+
+
+
